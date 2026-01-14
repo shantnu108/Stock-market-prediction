fe166b14a14a1b5c013198d33ff9ce58b001394b
shantnu108 <shantnuswami1008@gmail.com>
2026-01-14 17:14:33 +0530

    fix frontend api urls for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,19 @@
+// import axios from "axios";
+
+// export const getPrediction = async (symbol) => {
+//   const res = await axios.post(
+//     "http://localhost:5000/api/predict",
+//     { symbol }
+//   );
+//   return res.data.data;
+// };
 import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const getPrediction = async (symbol) => {
   const res = await axios.post(
-    "http://localhost:5000/api/predict",
+    `${BACKEND_URL}/api/predict`,
     { symbol }
   );
   return res.data.data;
--- frontend/src/services/historyApi.js
+++ frontend/src/services/historyApi.js
@@ -1,8 +1,24 @@
+// import axios from "axios";
+
+// export const getHistory = async (symbol, days) => {
+//   const res = await axios.get(
+//     "http://localhost:5000/api/history",
+//     {
+//       params: { symbol, days }
+//     }
+//   );
+//   return res.data;
+// };
+
+
+
 import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const getHistory = async (symbol, days) => {
   const res = await axios.get(
-    "http://localhost:5000/api/history",
+    `${BACKEND_URL}/api/history`,
     {
       params: { symbol, days }
     }
